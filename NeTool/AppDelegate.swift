a63a974f7d7859c76918005785e1fc97f9f5ebd2
Liu Tao <taliu@microstrategy.com>
2018-09-15 12:09:16 +0800

    Initial Commit

---
+++ NeTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NeTool
+//
+//  Created by Liu, Tao (Toni) on 9/15/18.
+//  Copyright © 2018 Liu, Tao (Toni). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NeTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NeTool
+//
+//  Created by Liu, Tao (Toni) on 9/15/18.
+//  Copyright © 2018 Liu, Tao (Toni). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
